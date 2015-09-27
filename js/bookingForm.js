cf4786199dcca39f65ff8d3a377587ae5108b9cf
callumJHays <callumjhays@gmail.com>
2015-09-28 00:01:29 +1000

    played around with csv

--- js/bookingForm.js
+++ js/bookingForm.js
@@ -2,26 +2,27 @@ var finishtime;
 $(function(){
 	$(document).on('change', "#service", function(){
 		var hasPetCount = $(this).parent().parent().find(".checkbox").length;
-		if($(this).val() == "Exterior only"){
+		if($(this).val() == "exterior_only"){
 			if(hasPetCount != 0){
 				$(this).parent().parent().find(".checkbox").fadeOut('slow', function(){$(this).remove()});
 			}
 		}else{
 			if(hasPetCount == 0){
 				$('<div class="checkbox col-sm-1"><label class="control-label" for="hasPet"><input type="checkbox" id="hasPet" value="">Had Pet?</label></div>')
-					.insertAfter($(this).parent())
+					.insertAfter($(this).parent());
 			}
 		}
 	});
 
 	// use carquery api to populate makeList
 	var makeList = ["Abarth","AC","Acura","Alfa Romeo","Allard","Alpina","Alpine","Alvis","AMC","Ariel","Armstrong Siddeley","Ascari","Aston Martin","Audi","Austin","Austin-Healey","Autobianchi","Auverland","Avanti","Beijing","Bentley","Berkeley","Bitter","Bizzarrini","BMW","Brilliance","Bristol","Bugatti","Buick","Cadillac","Caterham","Checker","Chevrolet","Chrysler","Citroen","Dacia","Daewoo","DAF","Daihatsu","Daimler","Datsun","De Tomaso","DKW","Dodge","Donkervoort","Eagle","Fairthorpe","Ferrari","Fiat","Fisker","Ford","GAZ","Geely","Ginetta","GMC","Holden","Honda","Hudson","Humber","Hummer","Hyundai","Infiniti","Innocenti","Isuzu","Italdesign","Jaguar","Jeep","Jensen","Kia","Koenigsegg","Lada","Lamborghini","Lancia","Land Rover","Lexus","Lincoln","Lotec","Lotus","Luxgen","Mahindra","Marcos","Maserati","Matra-Simca","Maybach","Mazda","MCC","McLaren","Mercedes-Benz","Mercury","MG","Mini","Mitsubishi","Monteverdi","Moretti","Morgan","Morris","Nissan","Noble","NSU","Oldsmobile","Opel","Packard","Pagani","Panoz","Peugeot","Pininfarina","Plymouth","Pontiac","Porsche","Proton","Reliant","Renault","Riley","Rolls-Royce","Rover","Saab","Saleen","Samsung","Saturn","Scion","Seat","Simca","Singer","Skoda","Smart","Spyker","SsangYong","SSC","Steyr","Studebaker","Subaru","Sunbeam","Suzuki","Talbot","Tata","Tatra","Tesla","Toyota","Trabant","Triumph","TVR","Vauxhall","Vector","Venturi","Volkswagen","Volvo","Wartburg","Westfield","Willys-Overland","Xedos","Zagato","Zastava","ZAZ","Zenvo","ZIL"],
-		apiURL = "http://www.carqueryapi.com/api/0.3/?callback=?";
+		carQueryApiURL = "http://www.carqueryapi.com/api/0.3/?callback=?";
 
 	// update the select list with makelist
 	var vehicleMakeOptionsHtml;
 	var jobLengthHours = [];
 	var jobPrices = [];
+
 	$.each(makeList, function(index, name){
 		vehicleMakeOptionsHtml += '<option>'+name+'</option>';
 	});
@@ -31,18 +32,10 @@ $(function(){
 	function calculateQuote($vehicle){
 		jobPrices.splice($vehicle.find('#quote').val());
 		$vehicle.find('#quote').html("loading...");
-		$.getJSON(apiURL, {cmd:"getTrims", model:$vehicle.find('#vehicleModel').val()}, function(response){
-
+		$.getJSON(carQueryApiURL, {cmd:"getTrims", model:$vehicle.find('#vehicleModel').val()}, function(response){
 			if(response.Trims[0].model_body == null){
-				// search for vehicle class 0
-				console.log('null found!');
-			}
-			else{
-				// use database to find the vehicle class given the model_body
-				var listOfBodyTypes = ["Large Cars", "Not Available", "Midsize Cars", "Sport Utility Vehicles",
-					"Standard Sport Utility Vehicles", "Subcompact Cars", "Compact Cars", "Minivan", "Two Seaters", 
-					"Standard Pickup Trucks", "Small Pickup Trucks", "Small Station Wagons", "Small Sport Utility Vehicles", 
-					"Cargo Vans", "Passenger Vans"]
+				// assume that the model body is not available and charge the max
+				response.Trims[0].model_body = "Not Available";
 			}
 			// use google sheets api to find the cost of vehicle class with specific service, and hadpet?
 			var totalCost = 40.50;
@@ -53,7 +46,7 @@ $(function(){
 	// use carquery api to populate the model list
 	function populateModelSelectList($vehicle){
 		$vehicle.find('#vehicleModel').html("loading...");
-		$.getJSON(apiURL, {cmd:"getModels", make:$vehicle.find('#vehicleMake').val().toLowerCase()}, function(response){
+		$.getJSON(carQueryApiURL, {cmd:"getModels", make:$vehicle.find('#vehicleMake').val().toLowerCase()}, function(response){
 			var vehicleModelOptionsHtml = "";
 			$.each(response.Models, function(index, model){
 				vehicleModelOptionsHtml += '<option>'+ model.model_name +'</option>';
